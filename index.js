dc56dbfe30b6d40113f64403b126a5bab8491063
Muhammed Midlaj PC <midlajpc531@gmail.com>
2024-08-19 13:13:25 +0530

    changed to switch

--- index.js
+++ index.js
@@ -2,28 +2,49 @@ const prompt = require("prompt-sync")();
 const n1 = parseFloat(prompt("enetr the 1st number:"));
 const operator = prompt("enter the operator:");
 const n2 = parseFloat(prompt("enter the 2nd number:"));
-if (operator === "+")
-{
-    console.log(n1 + n2);
+// if (operator === "+")
+// {
+//     console.log(n1 + n2);
     
-}
-else if (operator === "-")
-{
-    console.log(n1 - n2);
+// }
+// else if (operator === "-")
+// {
+//     console.log(n1 - n2);
     
-}
-else if(operator === "*")
-{
-    console.log(n1 * n2);
+// }
+// else if(operator === "*")
+// {
+//     console.log(n1 * n2);
     
-}
-else if(operator === "/")
-{
-    console.log(n1 / n2);
+// }
+// else if(operator === "/")
+// {
+//     console.log(n1 / n2);
     
-}
-else
-{
-    console.log("invalid input::");
+// }
+// else
+// {
+//     console.log("invalid input::");
     
+// }
+
+
+
+switch (operator)
+{
+    case "+":
+        console.log(n1 + n2);
+        break;
+     case "-":
+         console.log(n1 - n2);
+         break;   
+    case "*":
+        console.log(n1 * n2);
+        break;
+     case "/":
+         console.log(n1 / n2);
+         break;  
+    default:
+        console.log("invalid input");
+         
 }
